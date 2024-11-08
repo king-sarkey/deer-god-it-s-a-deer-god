85e024380236a4642f02413d97b64e4ab84e3997
king-sarkey <kadenvrtech@gmail.com>
2024-11-08 09:56:23 -0700

    Add files via upload

---
+++ turn based (2) (1).py
@@ -0,0 +1,14 @@
+import random
+class player:
+    health=100
+    mana=100
+
+    
+        pass
+def __init__(self.health.mana)
+self.health = health
+self.mana = mana
+class spells:
+    def damage():
+    def manna_cost():
+def spell "shay and slay" #summon thousands off b 
