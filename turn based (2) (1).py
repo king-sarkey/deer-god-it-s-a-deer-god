be900b9180b07861ae31fa4e5de101c0fa82fac2
king-sarkey <kadenvrtech@gmail.com>
2024-11-11 10:08:52 -0700

    add spell b!t¢|-|
    add spell b!t¢|-|

--- turn based (2) (1).py
+++ turn based (2) (1).py
@@ -3,12 +3,15 @@ class player:
     health=100
     mana=100
 
-    
         pass
 def __init__(self.health.mana)
 self.health = health
 self.mana = mana
 class spells:
-    def damage():
-    def manna_cost():
-def spell "shay and slay" #summon thousands off b 
+    damage=0
+    manna_cost=0
+    def __init__(self,damage,manna_cost):
+        self.damage=damage
+        self.manna_cost=manna_cost
+spell_shay_n_slay=spells(50,100)  #summon thousands of unionpacificbigboys and a shay locomotives
+spell_deer(50,100)   #summon thousands of unionpacificbigboys and a shay locomotives
